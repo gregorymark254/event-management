0f71208f664799920f930ec3ad26b62914f2bdc5
gregorymark254 <gregorymark254@gmail.com>
2024-06-15 12:53:09 +0300

    event

--- auth/models.py
+++ auth/models.py
@@ -10,7 +10,7 @@ class UserRoles(enum.Enum):
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
